74cf73ab602e43034554925b0a82051cd78455f5
Miguel Ccantuta <mccantuta@yahoo.com>
2025-01-03 19:42:54 -0500

    Adding AWS configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
+)
+
+var ginLambda *ginadapter.GinLambda
 
 func main() {
 	router := gin.Default()
 	router.GET("/users", GetUsers)
 	router.GET("/roles", GetRoles)
 	router.Run()
+
+	ginLambda = ginadapter.New(router)
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func GetUsers(c *gin.Context) {
